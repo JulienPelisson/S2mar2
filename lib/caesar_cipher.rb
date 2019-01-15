1e720ad26d04ae72cd6d7bd3c85f67d58fbb5ebd
JulienTHP <jpelisson@hotmail.fr>
2019-01-15 16:54:09 +0100

    multiple finis, Caesar commencé

---
+++ lib/caesar_cipher.rb
@@ -0,0 +1,6 @@
+def method1(str, num)
+	str2 = (str.ord + num)
+	puts str2.chr
+end
+
+method1("zob", 2)
---
+++ lib/multiples.rb
@@ -0,0 +1,41 @@
+def sum_of_3_or_5_multiples(n)
+	final_sum = 0
+	i = 1
+	
+	if n.class == String  
+		puts "Yo ! Je ne prends que les entiers naturels. TG."
+		return "Yo ! Je ne prends que les entiers naturels. TG."
+	elsif n.is_a?(Float) == true
+		puts "Yo ! Je ne prends que les entiers naturels. TG."
+		return "Yo ! Je ne prends que les entiers naturels. TG."
+	elsif n < 0 
+		puts "Yo ! Je ne prends que les entiers naturels. TG."
+		return "Yo ! Je ne prends que les entiers naturels. TG."
+	end
+	
+
+	 	while i < n  
+	final_sum += i 	if is_multiple_of_3_or_5?(i) == true 
+	i += 1
+	end
+	puts final_sum
+	return	final_sum
+	end
+
+	
+
+
+
+def is_multiple_of_3_or_5?(i)
+  
+  	if i % 3 == 0 || i % 5 == 0 
+  		return true
+  	else
+  		return false 
+ 	 end 	
+  end 
+   
+
+  
+sum_of_3_or_5_multiples("asdf")
+sum_of_3_or_5_multiples(10)
